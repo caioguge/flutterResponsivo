8c68d4d01185890bc5b2b45ec35e2eb6439065bb
Caio Guge <caioguge@gmail.com>
2024-01-08 12:31:26 -0300

    Criação da seção do topo para Tablet

--- lib/pages/home/widgets/sections/top_section.dart
+++ lib/pages/home/widgets/sections/top_section.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:layout_builder_na_pratica/breakpoints.dart';
 import 'package:layout_builder_na_pratica/pages/home/widgets/custom_search_field.dart';
 
 class TopSection extends StatelessWidget {
@@ -64,6 +65,63 @@ class TopSection extends StatelessWidget {
             ),
           );
         }
+
+        if (maxWidth >= mobileBreakPoint) {
+          return SizedBox(
+            height: 320,
+            child: Stack(
+              children: [
+                SizedBox(
+                  height: 250,
+                  width: double.infinity,
+                  child: Image.network(
+                    'https://images.pexels.com/photos/892757/pexels-photo-892757.jpeg?auto-compress',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Positioned(
+                  left: 20,
+                  top: 20,
+                  child: Card(
+                    color: Colors.black,
+                    elevation: 8,
+                    child: Container(
+                      width: 350,
+                      padding: const EdgeInsets.all(22),
+                      child: const Column(
+                        children: [
+                          Text(
+                            'Aprenda Flutter com este curso!',
+                            style: TextStyle(
+                              fontSize: 33,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Text(
+                            'Bora aprender Flutter com o professor Daniel Ciolfi! Cursos por apenas R\$22,90. Qualidade garantida.',
+                            style: TextStyle(
+                              fontSize: 15,
+                              color: Colors.white,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 16,
+                          ),
+                          CustomSearchField(),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        }
+
         return Container();
       },
     );
